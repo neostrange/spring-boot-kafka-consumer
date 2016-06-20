5252e11dd921cf0d412645a45fd8d567820df273
YG <yumnaghazi@gmail.com>
2016-06-20 13:29:16 +0500

    Changed avro decoder to string decoder

--- spring-boot-kafka-consumer/src/main/java/com/example/MessageProcessor.java
+++ spring-boot-kafka-consumer/src/main/java/com/example/MessageProcessor.java
@@ -8,59 +8,61 @@ import org.springframework.messaging.Message;
 
 public class MessageProcessor {
 
-	public void processKafkaMessage(Message<Vote> message) {
+	public void processKafkaMessage(Message<String> message) {
 
-		Vote receivedMessage;
+		String receivedMessage;
 
 		receivedMessage = message.getPayload();
 
-		System.out.println("message sent via http request: " + receivedMessage.getName());
+		System.out.println("message sent via http request: " + receivedMessage);
 
 	}
-	public <K, V> void processKafkaMessage(Map<String, Vote> payload) throws InterruptedException {
+	public <K, V> void processKafkaMessage(Map<String, String> payload) throws InterruptedException {
 		String key = null;
 		// for (K item : payload.keySet()) {
 		// key = (String) item;
 		// }
 
 		// Map<K, V> topic = (Map<K, V>) payload.get(key);
-		Vote vote = payload.get("votes");
+		String vote = payload.get("votes");
 		// List<Vote> votes = (List<Vote>) topic.get(0);
 
 		Thread.sleep(500);
 
-		System.out.println("vote: " + vote.getName() + "\n in thread:" + Thread.currentThread().getName());
+		System.out.println("vote: " + vote + "\n in thread:" + Thread.currentThread().getName());
 
 	}
 
 	@ServiceActivator
-	public void processKafkaVoteMessage(Map<String, Map<Integer, List<Vote>>> payload) {
+	public void processKafkaVoteMessage(Map<String, Map<Integer, List<String>>> payload) {
 		String key = null;
 		for (String item : payload.keySet()) {
 			key = (String) item; // topic
 
-			Map<Integer, List<Vote>> messages = payload.get(key);
+			Map<Integer, List<String>> messages = payload.get(key);
 			// Integer i;
 			for (Integer partition : messages.keySet()) { // partitions
 
 				// i = (Integer)partition;
 
-				List<Vote> values = (List<Vote>) messages.get(partition);
+				List<String> values = (List<String>) messages.get(partition);
 
-				for (Object v : values) {
-					if (v.getClass().getName().contains("Vote")) {
-						System.out.println("the type of the object is : " + v.getClass().getName() + "  ");
-						Vote object = (Vote) v;
-						System.out.println(" getting key as " + key + " partition as : " + partition + " and value is "
-								+ object.getName() + " within thread:" + Thread.currentThread().getName());
-					}
+				for (String v : values) {
+					System.out.println("Partition ["+ partition +"], Topic ["+ key +"], Data [" +v+"]" );
 					
-					if (v.getClass().getName().contains("Voter")) {
-						System.out.println("the type of the object is : " + v.getClass().getName() + "  ");
-						Voter object = (Voter) v;
-						System.out.println(" getting key as " + key + " partition as : " + partition + " and value is "
-								+ object.getName() + " within thread:" + Thread.currentThread().getName());
-					}
+//					if (v.getClass().getName().contains("Vote")) {
+//						System.out.println("the type of the object is : " + v.getClass().getName() + "  ");
+//						Vote object = (Vote) v;
+//						System.out.println(" getting key as " + key + " partition as : " + partition + " and value is "
+//								+ object.getName() + " within thread:" + Thread.currentThread().getName());
+//					}
+//					
+//					if (v.getClass().getName().contains("Voter")) {
+//						System.out.println("the type of the object is : " + v.getClass().getName() + "  ");
+//						Voter object = (Voter) v;
+//						System.out.println(" getting key as " + key + " partition as : " + partition + " and value is "
+//								+ object.getName() + " within thread:" + Thread.currentThread().getName());
+//					}
 				}
 			}
 		}
