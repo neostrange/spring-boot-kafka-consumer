8f14e66ffe2a68f7834ce68611d9fa17a4e93c7a
YG <yumnaghazi@gmail.com>
2016-06-21 13:58:14 +0500

    spring data redis integration

--- spring-boot-kafka-consumer/src/main/java/com/example/Feed.java
+++ spring-boot-kafka-consumer/src/main/java/com/example/Feed.java
@@ -1,8 +1,17 @@
 package com.example;
 
+import java.io.Serializable;
 import java.util.Date;
+import java.util.Map;
 
-public class Feed {
+public class Feed implements Serializable {
+	
+	
+	public enum TTPType{C2, MALWARE, SCANNING, WEB, SSH};
+	
+	public enum SuggestedCOA{BLOCK, MONITOR};
+	
+	public enum Type{IP, MD5, URL};
 	
 	private String indicator;
 	
@@ -12,9 +21,9 @@ public class Feed {
 	
 	private String description;
 	
-	private Date firstSeen;
+	private String firstSeen;
 	
-	private Date lastSeen;
+	private String lastSeen;
 	
 	private Date timestamp;
 	
@@ -24,14 +33,145 @@ public class Feed {
 	
 	private float confidence;
 	
+	private IncidentStats incidentStats;
+	
 	private String source;
 	
 	private String tlpLevel;
 	
-	public enum TTPType{C2, MALWARE, SCANNING, WEB, SSH};
+	private TTPType ttpType;
 	
-	public enum suggestedCOA{BLOCK, MONITOR};
+	private SuggestedCOA suggestedCOA;
+
+	public Feed(){}
 	
+	public Feed(String indicator, String type, String firstSeen, String lastSeen, String timestamp) {
+		this.indicator = indicator;
+		this.type = type;
+		this.firstSeen = firstSeen;
+		this.lastSeen = lastSeen;
+	}
+
+	public String getIndicator() {
+		return indicator;
+	}
+
+	public void setIndicator(String indicator) {
+		this.indicator = indicator;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getFirstSeen() {
+		return firstSeen;
+	}
+
+	public String setFirstSeen(String firstSeen) {
+		return this.firstSeen = firstSeen;
+	}
+
+	public String getLastSeen() {
+		return lastSeen;
+	}
+
+	public void setLastSeen(String lastSeen) {
+		this.lastSeen = lastSeen;
+	}
+
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public String getValidityPeriod() {
+		return validityPeriod;
+	}
+
+	public void setValidityPeriod(String validityPeriod) {
+		this.validityPeriod = validityPeriod;
+	}
+
+	public float getRiskFactor() {
+		return riskFactor;
+	}
+
+	public void setRiskFactor(float riskFactor) {
+		this.riskFactor = riskFactor;
+	}
+
+	public float getConfidence() {
+		return confidence;
+	}
+
+	public void setConfidence(float confidence) {
+		this.confidence = confidence;
+	}
+
+	public IncidentStats getIncidentStats() {
+		return incidentStats;
+	}
+
+	public void setIncidentStats(IncidentStats incidentStats) {
+		this.incidentStats = incidentStats;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getTlpLevel() {
+		return tlpLevel;
+	}
+
+	public void setTlpLevel(String tlpLevel) {
+		this.tlpLevel = tlpLevel;
+	}
+
+	public TTPType getTtpType() {
+		return ttpType;
+	}
+
+	public void setTtpType(TTPType ttpType) {
+		this.ttpType = ttpType;
+	}
+
+	public SuggestedCOA getSuggestedCOA() {
+		return suggestedCOA;
+	}
+
+	public void setSuggestedCOA(SuggestedCOA suggestedCOA) {
+		this.suggestedCOA = suggestedCOA;
+	}
+
 	
 	
 	
--- spring-boot-kafka-consumer/src/main/java/com/example/MessageProcessor.java
+++ spring-boot-kafka-consumer/src/main/java/com/example/MessageProcessor.java
@@ -1,12 +1,25 @@
 package com.example;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.Message;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 public class MessageProcessor {
+
+	@Autowired
+	FeedRepository feedRepo;
+	
+	public static Calendar calendar = Calendar.getInstance();
+	
+	public static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
 
 	public void processKafkaMessage(Message<String> message) {
 
@@ -17,6 +30,7 @@ public class MessageProcessor {
 		System.out.println("message sent via http request: " + receivedMessage);
 
 	}
+
 	public <K, V> void processKafkaMessage(Map<String, String> payload) throws InterruptedException {
 		String key = null;
 		// for (K item : payload.keySet()) {
@@ -36,33 +50,69 @@ public class MessageProcessor {
 	@ServiceActivator
 	public void processKafkaVoteMessage(Map<String, Map<Integer, List<String>>> payload) {
 		String key = null;
+		String ip, md5, url = null;
+		Feed tmpFeed = null;
 		for (String item : payload.keySet()) {
-			key = (String) item; 
+			key = (String) item;
 
 			Map<Integer, List<String>> messages = payload.get(key);
 			// Integer i;
-			for (Integer partition : messages.keySet()) { 
+			for (Integer partition : messages.keySet()) {
 				// partitions
 
 				List<String> values = (List<String>) messages.get(partition);
 
 				for (String v : values) {
-					System.out.println("Partition ["+ partition +"], Topic ["+ key +"], Data [" +v+"]" );
 
-					
-//					if (v.getClass().getName().contains("Vote")) {
-//						System.out.println("the type of the object is : " + v.getClass().getName() + "  ");
-//						Vote object = (Vote) v;
-//						System.out.println(" getting key as " + key + " partition as : " + partition + " and value is "
-//								+ object.getName() + " within thread:" + Thread.currentThread().getName());
-//					}
-//					
-//					if (v.getClass().getName().contains("Voter")) {
-//						System.out.println("the type of the object is : " + v.getClass().getName() + "  ");
-//						Voter object = (Voter) v;
-//						System.out.println(" getting key as " + key + " partition as : " + partition + " and value is "
-//								+ object.getName() + " within thread:" + Thread.currentThread().getName());
-//					}
+					System.out.println("Partition [" + partition + "], Topic [" + key + "], Data [" + v + "]");
+
+					JsonNode node = null;
+					try {
+						node = CustomObjectMapper.getInstance().readTree(v);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+					if (node.has("origin")) {
+						ip = node.get("origin").get("ip").asText();
+						tmpFeed = feedRepo.findFeed(ip);
+						if (tmpFeed != null) {
+							System.out.println("FOUND IT!:" + tmpFeed.getIndicator());
+							tmpFeed.setLastSeen(node.get("dateTime").asText());
+							feedRepo.updateFeed(tmpFeed);
+						} else {
+							tmpFeed = new Feed(ip, "ip", node.get("dateTime").asText(), node.get("dateTime").asText(), format.format(calendar.getTime()));
+							feedRepo.saveFeed(tmpFeed);
+						}
+						System.out.println("IP [" + ip + "]");
+					}
+					if (node.has("download")) {
+						md5 = node.get("download").get("md5Hash").asText();
+						tmpFeed = feedRepo.findFeed(md5);
+						if (tmpFeed != null) {
+							System.out.println("FOUND IT!:" + tmpFeed.getIndicator());
+							tmpFeed.setLastSeen(node.get("dateTime").asText());
+							feedRepo.updateFeed(tmpFeed);
+						} else {
+							tmpFeed = new Feed(md5, "md5", node.get("dateTime").asText(), node.get("dateTime").asText(), new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(Calendar.getInstance().getTime()));
+							feedRepo.saveFeed(tmpFeed);
+						}
+						//url
+						url = node.get("download").get("url").asText();
+						tmpFeed = feedRepo.findFeed(url);
+						if (tmpFeed != null) {
+							System.out.println("FOUND IT!:" + tmpFeed.getIndicator());
+							tmpFeed.setLastSeen(node.get("dateTime").asText());
+							feedRepo.updateFeed(tmpFeed);
+						} else {
+							tmpFeed = new Feed(md5, "url", node.get("dateTime").asText(), node.get("dateTime").asText(), new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(Calendar.getInstance().getTime()));
+							feedRepo.saveFeed(tmpFeed);
+						}
+						
+						System.out.println("md5 [" + md5 + "], url [" + url + "]");
+
+					}
 				}
 			}
 		}
